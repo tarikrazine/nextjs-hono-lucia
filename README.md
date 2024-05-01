e2a008df2acf6e40f0d25f79559d35e4b5391dab
tarik razine <tarikrazine.com@gmail.com>
2024-05-01 17:24:25 +0100

    feat: add readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Next.js 14 with Hono and Lucia v3 Auth boilerplate
+
+## Features
+
+- Authentication with Credentials Provider
+- Email verification *Input OTP 
+
+## Tools
+
+- Next.js 14
+- Hono
+- TypeScript
+- Lucia v3
+- Drizzle
+- Turso Sqlite
+- Tailwind CSS
+- Shadcn
+- resend
+
+## Deployments
+
+- Cloudflare 
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,13 +1,25 @@
 import { validateRequest } from "@/lib/user.server";
+import { LogIn, User2 } from "lucide-react";
+import Link from "next/link";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export default async function Home() {
-  const {user} = await validateRequest()
+  const { user } = await validateRequest();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {JSON.stringify(user, null, 2)}
+    <main className="flex justify-center items-center h-screen">
+      {user ? (
+        <div className="flex flex-col gap-y-4">
+          <p className="text-2xl font-bold">Welcome {user.email}</p>
+          {user && JSON.stringify(user, null, 2)}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-y-4">
+          <Link href="/register" className="flex bg-zinc-900 text-zinc-100 p-4 rounded-md"><User2 className="w-6 h-6 mr-2" />Register</Link>
+          <Link href="/login" className="flex bg-zinc-900 text-zinc-100 p-4 rounded-md"><LogIn className="w-6 h-6 mr-2" />Login</Link>
+        </div>
+      )}
     </main>
   );
 }
